229dd9d39634de1a83053c059a1687db2d33bf2f
ElvisMw <elvismwanthi@gmail.com>
2024-07-03 14:37:18 +0300

    revert changes

--- school_management_system/registration_app/urls.py
+++ school_management_system/registration_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register_student', views.student_registration_view, name='student-registration'),
     path('register_staff', views.staff_registration_view, name='staff-registration'),
     path('register_admin', views.admin_registration_view, name='admin-registration'),
     path('registration', views.registration_view, name = 'registration'),
-    # Add more paths as needed
+    path('users', views.user_list_view, name='user_list'),
+
 ]
