e68be77bc1e28d3681e8697eebbedbad3dfe2c35
ElvisMw <elvismwanthi@gmail.com>
2024-07-10 14:35:32 +0300

    deleted:    School Management System.session.sql

--- school_management_system/registration_app/views.py
+++ school_management_system/registration_app/views.py
@@ -19,9 +19,9 @@ def login_user(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
             password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
+            user = authenticate(email=email, password=password)
             if user is not None:
                 login(request, user)
                 return redirect('registration_app:user_list')  # Redirect to user list
