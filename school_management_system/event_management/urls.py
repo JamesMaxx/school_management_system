5a9d80d1e7af260428a07e6bf2649f3fa90ebed6
ElvisMw <elvismwanthi@gmail.com>
2024-07-10 14:52:18 +0300

    Fixed url paths in navbar, landing, and home pages.

--- school_management_system/event_management/urls.py
+++ school_management_system/event_management/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'event_management'
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('landing_page', views.landingpage, name='landing-page'),
     path('<int:year>/<str:month>/', views.home, name='home'),
-    path('events', views.all_events, name='list-events'),
+    path('events_list', views.all_events, name='events_list'),
     path('add_venue', views.add_venue, name='add-venue'),
     path('list_venues', views.list_venues, name='list-venues'),
     path('show_venue/<venue_id>', views.show_venue, name='show-venue'),
