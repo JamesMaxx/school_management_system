b1a587a82f22076c6a3f9bd77ef6508481cd3a63
ElvisMw <elvismwanthi@gmail.com>
2024-07-11 19:30:43 +0300

    updated login form

--- school_management_system/registration_app/admin.py
+++ school_management_system/registration_app/admin.py
@@ -6,9 +6,8 @@ class UserAdmin(BaseUserAdmin):
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_student', 'is_admin','is_superuser', 'groups', 'user_permissions')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_student', 'is_admin', 'is_superuser', 'groups')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
-        ('security', {'fields': ('password1', 'password2')}),
     )
     add_fieldsets = (
         (None, {
